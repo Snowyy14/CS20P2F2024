b70a4ff9b451fb891223d0ded6723dc83b24dcf4
38136012 <38136012@L815WD220019.edu.cbe.ab.ca>
2024-10-23 11:42:01 -0600

    11:41

--- Chapter6/src/SkillBuilders/Exponentiation.java
+++ Chapter6/src/SkillBuilders/Exponentiation.java
@@ -1,17 +1,20 @@
 package SkillBuilders;
 
+import java.nio.file.FileSystemNotFoundException;
+
 public class Exponentiation {
+	
+static int myMethod(int x, int y) {
+	return y + x;
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+	int z = myMethod (2, 3);
+	System.out.println(z);
+}
 
-		
-		
-		
-		
-		
-		
-		
-	}
+
+
+
 
 }
--- Chapter6/src/SkillBuilders/TimeConverter.java
+++ Chapter6/src/SkillBuilders/TimeConverter.java
@@ -1,10 +1,32 @@
 package SkillBuilders;
-
+import java.util.Scanner;
 public abstract class TimeConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		 try (Scanner userInput = new Scanner(System.in)) {
+			 
+			 Scanner scanner = new Scanner(System.in);
+			 
+		        //user input
+		        System.out.println("1. Hours to minutes");
+		        System.out.println("2. Days to hours");
+		        System.out.println("3. Minutes to Hours");
+		        System.out.println("4. Hours to Days");
+		        System.out.print("Please type the conversion you want: ");
+		        int input = scanner.nextInt();
+	
+		  
+	
+		        if (input == 1) {
+		        	System.out.println("hello");
+		        }
+		        else if (input == 2) { 
+		        	System.out.println("okay");
+		        }
+		        
+		        
+		 }
 	}
-
 }
