56d69e5b25e7cddd828e92358cd523c293ea607c
Snowyy14 <141593255+Snowyy14@users.noreply.github.com>
2024-10-31 10:23:28 -0600

    10:23

--- Chapter6/src/Mastery/AddCoins.java
+++ Chapter6/src/Mastery/AddCoins.java
@@ -4,8 +4,9 @@ public class AddCoins {
 
 	//method with user input
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in))  {
 
+        	
         	//asking for the number of coins they have
             System.out.print("Enter the number of pennies: ");
             int pennies = scanner.nextInt();
@@ -19,19 +20,22 @@ public class AddCoins {
             System.out.print("Enter the number of quaters: ");
             int quarters = scanner.nextInt();
 
-            String totalAmount = getDollarAmount(pennies, nickels, dimes, quarters);
+            String totalAmount = getdollaramount(pennies, nickels, dimes, quarters);
             
             //telling them how much money they have
             System.out.println("You have: " + totalAmount + " dollars");
-        }
+      }
     }
 
 //calling the method for the total from user input
-    public static String getDollarAmount(int pennies, int nickels, int dimes, int quarters) {
+    public static String getdollaramount(int pennies, int nickels, int dimes, int quarters) {
   
         double total = (pennies * 0.01) + (nickels * 0.05) + (dimes * 0.10) + (quarters * 0.25);
         
-     
-        return String.format("$%.2f", total);
-    }
+        return String.format("$" + total);
+        
+        
+        
+        
+}
 }
