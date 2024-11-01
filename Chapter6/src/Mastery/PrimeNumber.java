bf3d5ec440a1df542ade03d2202de5e9a5f962ab
38136012 <38136012@L815WD220019.edu.cbe.ab.ca>
2024-11-01 10:41:58 -0600

    10:41AM

--- Chapter6/src/Mastery/PrimeNumber.java
+++ Chapter6/src/Mastery/PrimeNumber.java
@@ -23,6 +23,8 @@ public class PrimeNumber {
 
     //this is the method to check if its prime
     public static boolean isPrime(int number) {
+    	
+    	//checks if the number is 1 or under if it is then its instantly false
         if (number <= 1) {
         	
         	
@@ -31,12 +33,14 @@ public class PrimeNumber {
         	
        }
         
-        
+        //start by checking if its divisible by 2 all the way to the square root of the number 
         for (int i = 2; i <= Math.sqrt(number); i++) {
+        	//checking for divisibility 
             if (number % i == 0) {
                 return false;
           }
         }
+        //if nothing is found its returned true
         return true;
  }
 }
