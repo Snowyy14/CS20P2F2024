4140be7f7c4fdeed70e1090b76383fcf08ac5fe1
Snowyy14 <141593255+Snowyy14@users.noreply.github.com>
2024-11-04 11:12:55 -0500

    11:10

--- Chapter6/src/Mastery/PrimeNumber.java
+++ Chapter6/src/Mastery/PrimeNumber.java
@@ -1,64 +1,39 @@
 package Mastery;
 import java.util.Scanner;
+
 public class PrimeNumber {
 
     public static void main(String[] args) {
         try (Scanner userInput = new Scanner(System.in)) {
 
-            //the user input
+            // The user input
             System.out.print("Please enter your number: ");
             int number = userInput.nextInt();
             
-            //the output for if its prime or not
+            // The output for whether it's prime or not
             if (isPrime(number)) {
                 System.out.println("It is a prime number");
-                
             } else {
-            	
                 System.out.println("It is not a prime number");
-                
-         }
-     }
+            }
+        }
     }
 
-    //this is the method to check if its prime
+    // This is the method to check if it's prime
     public static boolean isPrime(int number) {
-<<<<<<< HEAD
-    	
-    	//checks if the number is 1 or under if it is then its instantly false
+        // Checks if the number is 1 or below; if so, it's instantly false
         if (number <= 1) {
-        	
-        	
-           
-        	return false;
-        	
-       }
+            return false;
+        }
         
-        //start by checking if its divisible by 2 all the way to the square root of the number 
+        // Start by checking if it's divisible by 2 all the way to the square root of the number
         for (int i = 2; i <= Math.sqrt(number); i++) {
-        	//checking for divisibility 
+            // Checking for divisibility
             if (number % i == 0) {
                 return false;
-          }
+            }
         }
-        //if nothing is found its returned true
-=======
-        if (number <= 1) {
-        	
-        	
-           
-        	return false;
-        	
-       }
-        
-        
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-          }
-        }
->>>>>>> refs/remotes/origin/master
+        // If nothing is found, return true
         return true;
- }
+    }
 }
-
