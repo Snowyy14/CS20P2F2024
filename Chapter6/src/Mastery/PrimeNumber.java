5b14d5c3c377295893cc860917783e394108e675
Snowyy14 <141593255+Snowyy14@users.noreply.github.com>
2024-11-06 10:59:39 -0700

    10:58

--- Chapter6/src/Mastery/PrimeNumber.java
+++ Chapter6/src/Mastery/PrimeNumber.java
@@ -6,11 +6,11 @@ public class PrimeNumber {
     public static void main(String[] args) {
         try (Scanner userInput = new Scanner(System.in)) {
 
-            // The user input
+            //the user input
             System.out.print("Please enter your number: ");
             int number = userInput.nextInt();
             
-            // The output for whether it's prime or not
+            //the output for whether it's prime or not
             if (isPrime(number)) {
                 System.out.println("It is a prime number");
             } else {
@@ -19,21 +19,21 @@ public class PrimeNumber {
         }
     }
 
-    // This is the method to check if it's prime
+    //this is the method to check if it's prime
     public static boolean isPrime(int number) {
         // Checks if the number is 1 or below; if so, it's instantly false
         if (number <= 1) {
             return false;
         }
         
-        // Start by checking if it's divisible by 2 all the way to the square root of the number
+        //start by checking if it's divisible by 2 all the way to the square root of the number
         for (int i = 2; i <= Math.sqrt(number); i++) {
             // Checking for divisibility
             if (number % i == 0) {
                 return false;
             }
         }
-        // If nothing is found, return true
+        //if nothing is found, return true
         return true;
     }
 }
