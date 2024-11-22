41fdeae7372e2a4ee1caf56fc12d5d4a85c02c28
Snowyy14 <141593255+Snowyy14@users.noreply.github.com>
2024-11-22 10:42:38 -0700

    10:42

--- Chapter7/src/Mastery/LunchOrder.java
+++ Chapter7/src/Mastery/LunchOrder.java
@@ -3,70 +3,93 @@ package Mastery;
 import java.util.Scanner;
 
 class Food {
-	private double prices;
-	private int fat;
-	private int carbs;
-	private int fiber;
+    private double price;
+    private int fat;
+    private int carbs;
+    private int fiber;
 
-public food(double prices, int fat, int carbs, int fiber) {
-this.prices = prices;
-	this.fat = fat;
-	this.carbs = carbs;
-	this.fiber = fiber;
+  
+    public Food(double price, int fat, int carbs, int fiber) {
+        this.price = price;
+        this.fat = fat;
+        this.carbs = carbs;
+        this.fiber = fiber;
+    				}
+
+
+    public double getPrice() {
+        return price;
+    
+    
+    					}	
+    
+    public int getFat() {
+        return fat;
+    	}
+
+    public int getCarbs() {
+        return carbs;
+    }
+    
+    public int getFiber() {
+        return fiber;
+    	}
 }
 
-	public double gettingprice() {
-return prices;
-	}
+public class LunchOrder {
+    public static void main(String[] args) {
 
-		public int getingfat() {
-return fat;
-	}
+  
+        Food hamburger = new Food(1.85, 9, 33, 1);
+        Food salad = new Food(2.00, 1, 11, 5);
+        Food frenchFries = new Food(1.30, 11, 36, 4);
+        Food soda = new Food(0.95, 0, 38, 0);
 
-		public int gettingcarbs() {
-return carbs;
-	}
+   
+        Scanner scanner = new Scanner(System.in);
 
-		public int gettingfiber() {
-return fiber;
-	}
-	
-}		
-		
-		public class LunchOrder {
-			public static void main(String[] args) {
+        System.out.println("Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0g of fiber");
+        System.out.print("Please enter the number of hamburgers: ");
+        int numHamburgers = scanner.nextInt();
+        
+        
+        System.out.println("Each salad has 1.0g of fat, 11.0g of carbs, and 5.0g of fiber");
+        System.out.print("Please enter the number of salads: ");
+        int numSalads = scanner.nextInt();
 
-				food hamburger = new Food(1.85, 9, 33,1);
-					food salad = new Food(2.00, 1, 11, 5);
-					food frenchfries = new Food(1.30,11,36,4);
-					food soda = new Food(0.95,0,38,0);
+        
+        
+        System.out.println("French fries have 11.0g of fat, 36.0g of carbs, and 4.0g of fiber");
+        System.out.print("Please enter the number of french fries: ");
+        int numFries = scanner.nextInt();
 
-	Scanner scanner = new Scanner(System.in);
-
-				System.out.println("Please enter the number of hamburgers: );
-				System.out.printf("Each hamburger has 9.0g of fat, 33.0g of carbs, and 1.0g of fiber");
-	int numhamburgers = scanner.nextInt();
-
-								System.out.println("Please enter the number of salad: );
-				System.out.printf("Each salad has 1.0g of fat, 11.0g of carbs, and 5.0g of fiber");
-	int numsalads = scanner.nextInt();
-
-								System.out.println("Please enter the number of french fries: );
-				System.out.printf("French fries have 11.0g of fat, 36.0g of carbs, and 4.0g of fiber");
-	int numfries = scanner.nextInt();
-
-								System.out.println("Please enter the number of soda: );
-				System.out.printf("Each soda has 0.0g of fat, 38.0g of carbs, and 0.0g of fiber");
-	int numsodas = scanner.nextInt();
+        
+        System.out.println("Each soda has 0.0g of fat, 38.0g of carbs, and 0.0g of fiber");
+        System.out.print("Please enter the number of sodas: ");
+        int numSodas = scanner.nextInt();
 
 
-				double totalcost = numhamburgers * hamburger.getprice() + numsalads * salad.getprice() + numfries * fries.getprice() + numsodas * sodas.getprice() + 
+        double totalCost = numHamburgers * hamburger.getPrice() + numSalads * salad.getPrice() + numFries * frenchFries.getPrice() + numSodas * soda.getPrice();
 
-				
-		}
-		
-		
-		
-	}
+      
+        int totalFat = numHamburgers * hamburger.getFat() + numSalads * salad.getFat() + numFries * frenchFries.getFat() + numSodas * soda.getFat();
 
-}
+        int totalCarbs = numHamburgers * hamburger.getCarbs() + numSalads * salad.getCarbs() + numFries * frenchFries.getCarbs() + numSodas * soda.getCarbs();
+
+        int totalFiber = numHamburgers * hamburger.getFiber() + numSalads * salad.getFiber() + numFries * frenchFries.getFiber() + numSodas * soda.getFiber();
+
+      
+        System.out.println("Your total order summary:");
+        System.out.println("Total cost: $"  + totalCost);
+        System.out.println("Total Fat: " + totalFat+ "g");
+        System.out.println("Total Carbs: " + totalCarbs+ "g");
+        System.out.println("Total Fiber: "+ totalFiber+ "g");
+
+        scanner.close();
+        
+        
+        
+        
+        
+   			}
+			}		
